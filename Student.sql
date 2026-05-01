8bb474e0ff4a08554addffe78780c75eea1c0bef
Adanya Bailey <your-email@villanova.edu>
2026-05-01 18:28:18 -0400

    Added enrollment submission and inserted data in other tables

--- Student.sql
+++ Student.sql
@@ -10,8 +10,11 @@ INSERT INTO Student VALUES (8, 'Faith', 'Widgins-Brown', 'fwidgins@villanova.edu
 INSERT INTO Student VALUES (6, 'Umu', 'Diallo', 'udiallo@villanova.edu', 'Comprehensive Science');
 INSERT INTO Student VALUES (13, 'Sasha', 'Caines', 'scaines@villanova.edu', 'Political Science');
 INSERT INTO Student VALUES (19, 'Sidney', 'Mbah', 'smbah@villanova.edu', 'Psychology');
-INSERT INTO Student VALUES (1, 'Aaliyah', 'Brown', 'aaliyah.brown@villanova.edu', 'Biology');
-INSERT INTO Student VALUES (2, 'Jordan', 'Smith', 'jordan.smith@villanova.edu', 'Mathematics');
-INSERT INTO Student VALUES (3, 'Maya', 'Johnson', 'maya.johnson@villanova.edu', 'Computer Science');
-INSERT INTO Student VALUES (4, 'Daniel', 'Lee', 'daniel.lee@villanova.edu', 'Engineering');
-INSERT INTO Student VALUES (5, 'Kayla', 'Davis', 'kayla.davis@villanova.edu', 'Nursing');
+INSERT INTO Student VALUES (1, 'Aaliyah', 'Brown', 'abrown@villanova.edu', 'Biology');
+INSERT INTO Student VALUES (2, 'Jordan', 'Smith', 'jsmith@villanova.edu', 'Mathematics');
+INSERT INTO Student VALUES (3, 'Maya', 'Johnson', 'mjohns@villanova.edu', 'Computer Science');
+INSERT INTO Student VALUES (4, 'Daniel', 'Lee', 'dlee@villanova.edu', 'Engineering');
+INSERT INTO Student VALUES (5, 'Kayla', 'Davis', 'kdavis@villanova.edu', 'Nursing');
+INSERT INTO Student VALUES (21, 'Nasir', 'Prum', ' nprum@villanova.edu', 'Computer Science');
+INSERT INTO Student VALUES (22, 'Riquel', 'Owusu', 'rowusu01@villanova.edu', 'Computer Science');
+INSERT INTO Student VALUES (20, 'Adanya', 'Bailey', 'abaile05@villanova.edu', 'Computer Science');
